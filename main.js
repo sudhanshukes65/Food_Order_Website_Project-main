ed27023356ef366711f156177034c40510fa9b86
Radhika Arora <aradhika880@gmail.com>
2022-08-16 03:47:16 +0530

    request_modal

--- main.js
+++ main.js
@@ -19,6 +19,9 @@ const preBtn = document.querySelectorAll('.pre-btn');
 const videoPlayer = document.querySelector('.video-player');
 const video = videoPlayer.querySelector('.video');
 const playBtn = videoPlayer.querySelector('.play-button');
+const openModalBtn = document.querySelectorAll('[data-modal-target]');
+const closeModalBtns = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 
 productContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
@@ -43,3 +46,39 @@ playBtn.addEventListener('click', (e) => {
         e.target.textContent = 'Arrow.png'
     }
 })
+
+// Request a dish modal
+openModalBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    }) 
+})
+
+closeModalBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+
